606269203379575133ea325f0f08419e219b665c
Timo Früh <timo.frueh@icloud.com>
2021-09-27 05:51:16 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#MLA at KST
+
+Ich schreibe momentan gerade an meiner Maturaarbeit und so kam ich auf die Idee, dieses kleine Skript zu schreiben, das den richtigen MLA-Zitierstandard produziert den wir an der KST benutzen.
+
+Um das Programm zu starten, geben Sie entweder `python3 run.py` ins Terminal ein, während Sie sich im Ordner befinden oder lassen Sie die Shell run.py ausführen, das sollte auf MacOS-Systemen auch funktionieren.
+
+Viel Spass!
---
+++ constants.py
@@ -0,0 +1,25 @@
+TITLE = """MLA at KST
+¯¯¯¯¯¯¯¯¯¯
+"""
+
+MAIN_PROMPT = """
+Wählen Sie bitte eine der folgenden Optionen:
+    a - Erstnennung eines Buches
+    b - Zweitnennung eines Buches
+    c - Quellen und Darstellungen aus dem Internet
+
+    q - Skript beenden
+
+Geben Sie bitte einen Buchstaben ein: """
+
+AUTHOR_PROMPT = "Wie lautet der Name des/der zitierten Autoren/in? (Nachname, Vorname) "
+AUTHOR_LASTNAME_PROMPT = "Wie lautet der Nachname des/der Autoren/in? "
+TITLE_PROMPT = "Wie lautet der Titel des zitierten Werks/Website? "
+SUBTITLE_PROMPT = "Wie lautet der Untertitel des Werks/Website? (leer lassen wenn keiner vorhanden) "
+EDITION_PROMPT = "Aus welcher Auflage des Werks stammt das Zizal? (bitte nur eine Zahl eingeben) "
+PUBLISHER_PROMPT = "Mit welchem Verlag wurde das zitierte Werk publiziert? "
+LOCATION_PROMPT = "Wo wurde das zitierte Werk publiziert? "
+YEAR_PROMPT = "In welchem Jahr wurde das zitierte Werk publiziert? "
+PAGES_PROMPT = "Welche Seite(n) des Werks werden zitiert? (bitte ohne \"S.\" eingeben) "
+URL_PROMPT = "Wie lautet die URL der Website? "
+DOWNDATE_PROMPT = "Wann haben Sie die Seite angesehen? "
