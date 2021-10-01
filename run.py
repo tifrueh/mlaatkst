298304a063ad10212fd612e2e040345ae8693225
Timo Früh <timo.frueh@icloud.com>
2021-10-02 00:46:08 +0200

    start coding

--- run.py
+++ run.py
@@ -2,10 +2,29 @@
 import constants as c
 import helpers as h
 import tkinter as tk
-from tkinter import tkk
+from tkinter import ttk
+from time import sleep
 
 def run():
-    nothing = {}
+    root = tk.Tk()
+    root.title("MLAatKST")
+    root.geometry("300x700+100+100")
+
+    menuPosition = 0
+
+    if menuPosition == 0:
+        h.startScreen(window = root)
+    
+    elif menuPosition == 1:
+        h.book(window = root)
+
+    elif menuPosition == 2:
+        h.bookScnd(window = root)
+
+    elif menuPosition == 3:
+        h.web(window = root)
+
+    root.mainloop()
 
 if __name__ == "__main__":
     run()
