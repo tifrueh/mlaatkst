0d0ffc1b43fb21e128fde3d060e152ebfa4d255b
Timo Früh <timo.frueh@icloud.com>
2021-10-04 17:28:50 +0200

    resize main window

--- App.py
+++ App.py
@@ -1,13 +1,14 @@
 import tkinter as tk
 import constants as c
 from tkinter import ttk
+from BookFrame import BookFrame
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
 
         self.title("MLA @ KST")
-        self.geometry("300x600+50+50")
+        self.geometry("600x600+50+50")
         self.resizable(False, False)
 
 if __name__ == "__main__":
