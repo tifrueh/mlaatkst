532865542650b210e8ebcccbc8f7b3fb215f0328
Timo Früh <timo.frueh@icloud.com>
2021-10-04 18:04:55 +0200

    update prompts

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 # this is the file where all constants are located
-AUTHOR_PROMPT = "Name des Autors:\n(Nachname, Vorname)"
-AUTHOR_LASTNAME_PROMPT = "Nachname des Autors:"
+AUTHOR_PROMPT = "Name des Autors / der Autorin:\n(Nachname, Vorname)"
+AUTHOR_LASTNAME_PROMPT = "Nachname des Autors / der Autorin:"
 TITLE_PROMPT = "Titel des Werks:"
 SUBTITLE_PROMPT = "Untertitel: \n(leer wenn nicht vorhanden)"
 EDITION_PROMPT = "Auflage: \n(Zahl eingeben)"
