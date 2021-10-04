b44a08888a3a0c6b5882a7a5049c7a40713e3be8
Timo Früh <timo.frueh@icloud.com>
2021-10-04 21:41:31 +0200

    update prompts

--- constants.py
+++ constants.py
@@ -1,9 +1,9 @@
 # this is the file where all constants are located
-AUTHOR_PROMPT = "Name des Autors / der Autorin:\n(Nachname, Vorname)"
+AUTHOR_PROMPT = "Name des Autors / der Autorin: (Nachname, Vorname)"
 AUTHOR_LASTNAME_PROMPT = "Nachname des Autors / der Autorin:"
 TITLE_PROMPT = "Titel des Werks:"
-SUBTITLE_PROMPT = "Untertitel: \n(leer wenn nicht vorhanden)"
-EDITION_PROMPT = "Auflage: \n(Zahl eingeben)"
+SUBTITLE_PROMPT = "Untertitel: (leer wenn nicht vorhanden)"
+EDITION_PROMPT = "Auflage: (Zahl eingeben)"
 PUBLISHER_PROMPT = "Verlag:"
 LOCATION_PROMPT = "Ort:"
 YEAR_PROMPT = "Jahr: "
