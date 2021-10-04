3f337d96539f0f6c1e65e3910bb2c0922c9b9fdc
Timo Früh <timo.frueh@icloud.com>
2021-10-04 16:22:44 +0200

    write App class and bootstrap

--- App.py
+++ App.py
@@ -2,3 +2,15 @@ import CitationFormatter
 import tkinter as tk
 from tkinter import ttk
 import constants as c
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("MLA @ KST")
+        self.geometry("300x600+50+50")
+        self.resizable(False, False)
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
