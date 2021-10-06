789980c7752fdda0faa78c957cb3079249240761
Timo Früh <timo.frueh@icloud.com>
2021-10-06 13:38:49 +0200

    add comments to main.py

--- src/main.py
+++ src/main.py
@@ -26,23 +26,32 @@ class App(tk.Tk):
     def __init__(self):
         super().__init__()
 
+        # show the background image bg2.png from the resources folder in a label
         self.appPath = os.path.dirname(__file__)
         self.imagePath = os.path.join(self.appPath, "resources", "bg2.png")
         self.bg = tk.PhotoImage(file=self.imagePath, format="png")
         self.bgLabel = ttk.Label(self, image=self.bg)
         self.bgLabel.place(x=0, y=0, relwidth=1, relheight=1)
 
+        # add window title from constants file
         self.title(c.TITLE)
         self.geometry("700x700+50+50")
         self.resizable(False, False)
 
+        # configure the one column of the grid view
         self.columnconfigure(index=0, weight=1)
 
+        # add a title label containing the title message
         self.titleLabel = ttk.Label(self, text=c.TITLE_LABEL, font=("Futura", 20))
         self.titleLabel.grid(column=0, row=0, pady=40)
 
 
 if __name__ == "__main__":
+    # start the main window specified above
     app = App()
+
+    # place the control frame inside the window
     ControlFrame(app)
+
+    # call tkinter mainloop of the main window
     app.mainloop()
