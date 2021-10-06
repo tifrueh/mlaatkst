b4f9839c57d4fa60a36b1845ea250e42bfdc27d8
Timo Früh <timo.frueh@icloud.com>
2021-10-06 13:59:17 +0200

    add comments to citation_formatter.py

--- src/citation_formatter.py
+++ src/citation_formatter.py
@@ -1,7 +1,10 @@
 class CitationFormatter:
 
+    # define the method for formatting a citation of a book
     @staticmethod
     def book(nameauthor, title, subtitle, edition, publisher, location, year, pages):
+
+        # omit subtitle if empty
         if subtitle == "" or subtitle == " ":
             result = f"{nameauthor}, {title}, {edition}. Auflage, {publisher}, {location}, {year}, S. {pages}."
         else:
@@ -9,14 +12,18 @@ class CitationFormatter:
 
         return result
 
+    # define the method for formatting the second citation of a book
     @staticmethod
     def scnd_book(lastnameauthor, year, pages):
         result = f"{lastnameauthor}, {year}, S. {pages}."
 
         return result
 
+    # define the method for formatting a citation of a website
     @staticmethod
     def web(nameauthor, title, subtitle, url, downdate):
+
+        # omit subtitle if empty
         if subtitle == "" or subtitle == " ":
             result = f"{nameauthor}, {title}, {url}, {downdate}."
         else:
