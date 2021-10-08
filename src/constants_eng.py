e0143b924bc5ca647e6ae587c8232ae3ea31fcc3
Timo Früh <timo.frueh@icloud.com>
2021-10-08 15:26:41 +0200

    rename constants and write english version

---
+++ src/constants_eng.py
@@ -0,0 +1,26 @@
+# this is the file where all constants are located
+
+AUTHOR_PROMPT = "Name of the author (name, surname)"
+AUTHOR_LASTNAME_PROMPT = "Family name of the author:"
+TITLE_PROMPT = "Title of the work:"
+SUBTITLE_PROMPT = "Subtitle: (leave empty if nonexistent)"
+EDITION_PROMPT = "Edition: (enter a number)"
+PUBLISHER_PROMPT = "Publisher:"
+LOCATION_PROMPT = "Location:"
+YEAR_PROMPT = "Year: "
+PAGES_PROMPT = "Cited page(s):"
+URL_PROMPT = "URL:"
+DOWNDATE_PROMPT = "Download date:"
+
+BOOK_BUTTON = "First mention of a book"
+SCNDBOOK_BUTTON = "Second mention of a book"
+WEB_BUTTON = "Mention of a website"
+DONE_BUTTON = "Complete footnote"
+COPY_BUTTON = "Copy footnote"
+RESET_BUTTON = "Reset entries"
+
+TITLE = "MLA @ KST"
+TITLE_LABEL = "MLA standard at the KST"
+CONTROL_FRAME_TITLE = "Options"
+
+STANDARD_OUTPUT = "The result appears here"
