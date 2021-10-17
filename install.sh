f90e5e50a6fe9af763ead84b2a47110bd5031e12
Timo Früh <timo.frueh@icloud.com>
2021-10-17 15:12:11 +0200

    add version support to install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd "$SCRIPT_DIR/src" || exit
+
 pyinstaller --clean --name="MLAatKST" --windowed \
   --icon="$SCRIPT_DIR/assets/mlaatkst.icns" \
   --osx-bundle-identifier="com.tifrueh.mlaatkst" \
@@ -10,5 +11,6 @@ pyinstaller --clean --name="MLAatKST" --windowed \
   --add-data="$SCRIPT_DIR/src/resources/lang.txt:./resources" \
   --workpath="$SCRIPT_DIR/out/build" \
   --distpath="$SCRIPT_DIR/out/dist" \
-  "$SCRIPT_DIR/src/main.py"
-  
+  "$SCRIPT_DIR/src/main.py" &&
+
+plutil -replace "CFBundleShortVersionString" -string "1.2.1" "$SCRIPT_DIR/out/dist/MLAatKST.app/Contents/Info.plist"
