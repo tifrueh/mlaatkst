2c4e6ccac43f04dc6a4d952b7b9bd8776f8649da
Timo Früh <timo.frueh@icloud.com>
2021-10-29 10:56:21 +0200

    update paths in install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-cd "$SCRIPT_DIR/src" || exit
+cd "$SCRIPT_DIR/mlaatkst" || exit
 
 pyinstaller --clean --name="MLAatKST" --windowed \
   --icon="$SCRIPT_DIR/assets/mlaatkst.icns" \
   --osx-bundle-identifier="com.tifrueh.mlaatkst" \
   --add-data="$SCRIPT_DIR/README.md:." \
   --add-data="$SCRIPT_DIR/LICENSE.md:." \
-  --add-data="$SCRIPT_DIR/src/resources/bg2.png:./resources" \
-  --add-data="$SCRIPT_DIR/src/resources/lang.txt:./resources" \
+  --add-data="$SCRIPT_DIR/mlaatkst/resources/bg2.png:./resources" \
+  --add-data="$SCRIPT_DIR/mlaatkst/resources/lang.txt:./resources" \
   --workpath="$SCRIPT_DIR/out/build" \
   --distpath="$SCRIPT_DIR/out/dist" \
-  "$SCRIPT_DIR/src/main.py" &&
+  "$SCRIPT_DIR/mlaatkst/main.py" &&
 
 plutil -replace "CFBundleShortVersionString" -string "dev" "$SCRIPT_DIR/out/dist/MLAatKST.app/Contents/Info.plist"
