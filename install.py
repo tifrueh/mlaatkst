722f001d6f926682ae7d4c79a453c0ba556673b8
Timo Früh <timo.frueh@icloud.com>
2022-02-19 17:55:17 +0100

    update installer

--- install.py
+++ install.py
@@ -10,16 +10,15 @@ icon_path = os.path.join(dir_path, "assets", "mlaatkst.icns")
 osx_bundle_identifier = "com.tifrueh.mlaatkst"
 readme_path = os.path.join(dir_path, "README.md")
 license_path = os.path.join(dir_path, "LICENSE.md")
-resources_path = os.path.join(dir_path, "mlaatkst", "resources")
+resources_path = os.path.join(dir_path, "resources")
 workpath = os.path.join(dir_path, "out", "build")
 distpath = os.path.join(dir_path, "out", "dist")
 
-main_path = os.path.join(dir_path, "mlaatkst.py")
+main_path = os.path.join(dir_path, "mlaatkst/__main__.py")
 
 plist_path = os.path.join(distpath, "MLAatKST.app", "Contents", "Info.plist")
 
 PyInstaller.__main__.run([
-    "--windowed",
     "--clean",
     f"--name={name}",
     f"--icon={icon_path}",
@@ -34,10 +33,14 @@ PyInstaller.__main__.run([
     "-y"
 ])
 
-with open(plist_path, "rb") as pl:
-    plist = plistlib.load(pl)
+try:
+    with open(plist_path, "rb") as pl:
+        plist = plistlib.load(pl)
 
-plist["CFBundleShortVersionString"] = "dev"
+    plist["CFBundleShortVersionString"] = "dev"
 
-with open(plist_path, "wb") as pl:
-    plistlib.dump(plist, pl)
+    with open(plist_path, "wb") as pl:
+        plistlib.dump(plist, pl)
+
+except FileNotFoundError:
+    print("ERROR: Property list not found; manual adjustment of version string necessary ...")
