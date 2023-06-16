6c2605549e2e99ac95cb40ef6c1825e6d469d7be
Timo Früh <timo.frueh@icloud.com>
2023-06-16 17:25:57 +0200

    add license

--- dist/macOS/bundle.sh
+++ dist/macOS/bundle.sh
@@ -20,6 +20,7 @@ mkdir -p $LIB_PATH
 
 cp ../../resources/Info.plist $CONTENTS_PATH
 cp ../../README.md $RES_PATH
+cp ../../LICENSE $RES_PATH
 cp ../../resources/mlaatkst.icns $RES_PATH
 cp ../../build/MLAatKST $EXE_PATH
 
