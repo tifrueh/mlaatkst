f125b7c0c9ce7ab68870a0167d83dec1da6d52a8
Timo Früh <timo.frueh@icloud.com>
2023-06-16 17:17:19 +0200

    create package aswell

--- dist/macOS/bundle.sh
+++ dist/macOS/bundle.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+VERSION=2.0.0
+
 APP=MLAatKST.app
+PKG=MLAatKST-v$VERSION.pkg
 CONTENTS_PATH=$APP/Contents
 RES_PATH=$CONTENTS_PATH/Resources
 EXE_PATH=$CONTENTS_PATH/macOS
@@ -8,6 +11,8 @@ LIB_PATH=$CONTENTS_PATH/Frameworks
 
 rm -rf $APP
 mkdir $APP
+
+rm $PKG
 
 mkdir -p $RES_PATH
 mkdir -p $EXE_PATH
@@ -19,3 +24,4 @@ cp ../../resources/mlaatkst.icns $RES_PATH
 cp ../../build/MLAatKST $EXE_PATH
 
 dylibbundler -od -b -x $EXE_PATH/MLAatKST -d $LIB_PATH -p @executable_path/../Frameworks
+pkgbuild --component $APP --install-location /Applications $PKG
