0eeb50b357d195045bb47e0b65438f82c9312cb3
Timo Früh <timo.frueh@icloud.com>
2023-06-23 19:45:06 +0200

    move extraction functionality to separate script

---
+++ resources/lang/extract.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+parent_path=$( cd "$(dirname "$0")" ; pwd -P )
+
+cd "$parent_path"
+
+xgettext -C -k_ --from-code=utf-8 \
+         -o ./de/LC_MESSAGES/messages.po \
+         --copyright-holder='Timo Früh' \
+         --package-name='MLAatKST' \
+         --msgid-bugs-address='timo.frueh@icloud.com' \
+         --join-existing \
+         ../../src/*
