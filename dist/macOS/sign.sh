f06e662688729f5b55947921235d58138c0580a1
Timo Früh <timo.frueh@icloud.com>
2023-07-07 23:26:06 +0200

    add signing script

---
+++ dist/macOS/sign.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+parent_path=$( cd "$(dirname "$0")" ; pwd -P )
+
+cd "$parent_path"
+
+echo "----=SIGNING=----"
+
+codesign --force --verbose=2 --sign $CERT_APPL MLAatKST.app
+
+echo "-----------------\n"
+
+echo "---=VERIFYING=---"
+
+codesign --verbose=2 --verify MLAatKST.app
+
+echo "-----------------\n"
