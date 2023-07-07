38e74769bfbdada51749f5d0115d16f24e228e44
Timo Früh <timo.frueh@icloud.com>
2023-07-07 23:17:00 +0200

    use install instead of cp and fix bundle structure

--- dist/macOS/bundle.sh
+++ dist/macOS/bundle.sh
@@ -8,20 +8,16 @@ cd "$parent_path"
 APP=MLAatKST.app
 CONTENTS_PATH=$APP/Contents
 RES_PATH=$CONTENTS_PATH/Resources
-EXE_PATH=$CONTENTS_PATH/macOS
+EXE_PATH=$CONTENTS_PATH/MacOS
 LC_DE_PATH=$RES_PATH/de.lproj
 
 rm -rf $APP
 mkdir $APP
 
-mkdir -p $RES_PATH
-mkdir -p $EXE_PATH
-mkdir -p $LC_DE_PATH
+install -d -m 755 $CONTENTS_PATH $RES_PATH $EXE_PATH $LC_DE_PATH
 
-cp ../../resources/Info.plist $CONTENTS_PATH
-cp ../../README.md $RES_PATH
-cp ../../LICENSE $RES_PATH
-cp ../../resources/mlaatkst.icns $RES_PATH
-cp ../../build/mlaatkst $EXE_PATH
-cp ../../resources/lang/de/LC_MESSAGES/mlaatkst.mo $LC_DE_PATH
-cp ../../resources/lang/de/LC_MESSAGES/wxstd.mo $LC_DE_PATH
+install -m 644 ../../resources/Info.plist $CONTENTS_PATH
+install -m 644 ../../resources/lang/de/LC_MESSAGES/mlaatkst.mo $LC_DE_PATH
+install -m 644 ../../resources/lang/de/LC_MESSAGES/wxstd.mo $LC_DE_PATH
+install -m 644 ../../resources/mlaatkst.png $RES_PATH
+install -s -m 755 ../../build/mlaatkst $EXE_PATH
